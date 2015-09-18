c41e9acddaedd907e80a088125c3fd936160eb4c
rails bhagath <rails@gmail1.com>
2015-09-18 11:32:51 +0530

    mobile check

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 #production.rb
-set :domain, "63.142.252.87"
+set :domain, "23.227.177.94"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/wholesale.git"
 set :deploydir, "/var/www/sunshiestorefl.com/public_html"
-set :user, "root"
+set :user, "shivraj"
 set :use_sudo, false
